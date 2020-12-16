a3a4c00fefa8ea8d98c06e4ca4e5be714376d1ec
starmlerp <filip.jamuljak@gmail.com>
2020-12-16 20:47:34 +0100

    started gradual migration from C++ to C standard library, in order to ensure backward compatibility with both

--- varfile.cpp
+++ varfile.cpp
@@ -2,11 +2,20 @@
 
 VarFile::VarFile(const char* fpathi){
 	fpath=(char*)fpathi;
+	FILE* f=fopen(fpath, "r");
+	if(f==NULL)f=fopen(fpath, "w");
+	fclose(f);
 }
 char* VarFile::path(){
 	return fpath;
 }
-
+/*
+void initarray(void* ptr, size_t size){
+	if(ptr!=NULL)free(ptr);
+	ptr=malloc(size+1);
+	ptr[size]=NULL;
+}
+*/
 int VarFile::get(const std::string& name, int* target){
 	FILE* file = fopen(fpath, "r");
 	int c;
@@ -57,34 +66,47 @@ int VarFile::get(const std::string& name, char** target){
 	return 0;
 };
 
-int VarFile::set(const std::string& name, const int target){
+int VarFile::set(const char* name, const int target){
 	FILE* file = fopen(fpath, "r+");
 	int c;
-	std::string word;
+	size_t wsize=0;
+	size_t tsize;
+	for(tsize=0;target[tsize];tsize++);
+	char* word;
 	do{
-		c=fgetc(file);
-		if(c==(int)'\n' || c==EOF || c==(int)' '){
-			if(word==name){
-				word="";
-				size_t ins=ftell(file);
-				do{c=fgetc(file);}while(c!=(int)'\n' || c!=EOF || c!=(int)' ');//find the point at which targeted entry ends
-				size_t hold=ftell(file);
-				fseek(file, 0L, SEEK_END);
-				char* buf=new char[ftell(file)-hold];//set the buffer size to the length after the data pointed to until the end of file
-				fseek(file, hold, SEEK_SET);
-				fread(buf, sizeof(buf), 1, file);//store the entries coming after the targeted entry
-				fseek(file, 0L, ins);
-				fprintf(file, "%d\n%s", target, buf);
+		c=fgetc(file);//read a single character
+		if((c==(int)'\n' || c==EOF || c==(int)' ')){//if its newline, end of file, of spacing
+			if(wsize==tsize){
+				int cmpwrd=1;
+				for(int i=0; cmpwrd && i<wsize; i++)cmpwrd=word[i]==target[i];//compare the 2 strings
+				if(cmpwrd){
+					free(word);
+					size_t ins=ftell(file);
+					do{c=fgetc(file);}while(c!=(int)'\n' || c!=EOF || c!=(int)' ');//find the point at which targeted entry ends
+					size_t hold=ftell(file);
+					fseek(file, 0L, SEEK_END);
+					char* buf=(char*)malloc[ftell(file)-hold];//set the buffer size to the length after the data pointed to until the end of file
+					fseek(file, hold, SEEK_SET);
+					fread(buf, sizeof(buf), 1, file);//store the entries coming after the targeted entry
+					fseek(file, 0L, ins);
+					fprintf(file, "%d\n%s", target, buf);
+				}
 			}
-			word="";
+			free(word);
 		}
-		else word+=(char)c;
+		else{ 
+			char* holder=NULL;
+			initarray(holder, wsize+1);
+			for(int i=0; i<wsize; i++)holder[i]=word[i];//copy word into holder
+			holder[++wsize]=(char)c;//add the read character
+			free(word);//deallocate word
+			word=holder;//put the location of holder array into word
+		}
 
 	}while(c!=EOF);
 	char* out;
 	fseek(file, 0, SEEK_END);
-	sprintf(out, "%s %d\n",name.c_str(), target);
-	fwrite(out, sizeof(out), 1, file);
+	fprintf(file, "%s %d\n",name, target);
 	fclose(file);
 	return 0;
 };
--- varfile.h
+++ varfile.h
@@ -5,15 +5,16 @@
 
 class VarFile{
 	char* fpath;
-	std::string parseString();
+	void initarray(const void* ptr);
 	public:
 	VarFile(const char* fpathi);
 	char* path();
+	
 	int get(const std::string& name, int* target);
 	int get(const std::string& name, float* target);
 	int get(const std::string& name, char** target);
 	
-	int set(const std::string& name, const int target);
+	int set(const char* name, const int target);
 	int set(const std::string& name, const float target);
 	int set(const std::string& name, const char* target);
 };
