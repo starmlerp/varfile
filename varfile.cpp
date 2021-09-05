2d8a724ae6a89d85e372fe27f52c4a443e31c8b1
starmlerp <filip.jamuljak@gmail.com>
2021-09-05 14:00:41 +0200

    performance improvementd

--- varfile.cpp
+++ varfile.cpp
@@ -12,13 +12,25 @@
 #define DEBUG
 
 int isFrivolous(char inT){//this is a private function, neccesary only for the function of this library, as such it is not neccesary to make it visible to the main code
-	for(unsigned long i=0; i < sizeof(FRIVOLOUS); i++)if(inT == FRIVOLOUS[i])return 1;
+	for(unsigned long i=0; FRIVOLOUS[i]; i++)if(inT == FRIVOLOUS[i])return 1;
 	return 0;
 }
 
 avf::Entry* avf::load(FILE* target){
 	rewind(target);
-	avf::Entry* object = new avf::Entry[1];
+	unsigned long outsize = 0;
+	char inC;
+	do{
+		inC = fgetc(target);
+		if(inC == TERMINATOR || inC == BRACKETS[0])outsize++; // count up all of the entries
+	}while(inC != EOF);// runs through the whole file
+	#ifdef DEBUG
+	printf("counted entries: %ld\n", outsize);
+	#endif
+	rewind(target);
+	inC = 0;
+	unsigned long outpos = 0;
+	avf::Entry* object = new avf::Entry[outsize+1];
 	object[0].name = NULL;
 	//TODO: implement proper parsing to detect invalid variable names (starting with numbers, including non-aphlanumerical characters etc.)
 	
@@ -43,21 +55,23 @@ avf::Entry* avf::load(FILE* target){
 	unsigned long objvsize=0;
 	char* objname;
 	char* objvalue; 	
-	std::stack<avf::Entry*> objects;	
+	std::stack<avf::Entry*> objects;
+	inC = fgetc(target);
 	while(true){
 		// we are entering an infinite loop, since the following code is going to be implemented as a state machine
 		// the loop is going to be broken on a return call
-		char inC;
-		if(state)inC = fgetc(target);
+		//if(state)inC = fgetc(target);
 		#ifdef DEBUG
-		printf("input: %c, state: %d\n", inC=='\n'?'N':inC, state);
+		printf("input: %c(%d), state: %d\n", inC=='\n'?'N':inC, (int)inC, state);
 		fflush(stdout);
 		#endif
 		if(inC=='\n')line++;//keep track of lines, will be used to display errors
 		if(inC==EOF){
-			if(state==SI)return object;
+			if(state==SI){
+				return object;
+			}
 			else{
-				free(object);
+				delete [] object;
 				object = new avf::Entry[1];
 				object[0].name = new char[1];
 				object[0].name[0]=0;
@@ -69,23 +83,25 @@ avf::Entry* avf::load(FILE* target){
 		}
 		switch(state){
 			case SI:
-				if(isFrivolous(inC))break;
-				else if(inC == BRACKETS[1]){
-					objects.pop();
-					break;
+				if(isFrivolous(inC))inC = fgetc(target);
+				else if(inC == BRACKETS[1]){ // if brace closing is found
+					objects.pop();//pop the stack
+					inC = fgetc(target);
 				}
-				else {
+				else{
 					state = ID;
 				}// otherwise assume we reached an identifier
+				break;
 			
 			case ID:
 				if(objnsize==2){
 					//TODO: following condition is impossible to meet. fix that
 					if(objname[0] == '/' && objname[1] == '/'){// if the input appears to be a start of comment
 						objnsize=0;
-						free(objname);
+						delete [] objname;
 						objname=NULL;
 						state = CM;// mark it as such
+						inC=fgetc(target);
 						break;
 					}
 				}
@@ -94,61 +110,49 @@ avf::Entry* avf::load(FILE* target){
 					//holder is an additional character long in order to hold null terminator
 					for(unsigned long i = 0; i < objnsize-1;i++)holder[i]=objname[i];
 					holder[objnsize-1]=inC;
-					if(objnsize > 1)free(objname);
+					if(objnsize > 1)delete [] objname;
 					objname=holder;//dirty dirty append
+					inC = fgetc(target);
 					break;
 				}
 
 				else if(isFrivolous(inC)){//end of identifier
 					objname[objnsize]='\0';
 					state=CR;//assume we are looking for a correlator
+					break;
 				}
 
 				else {
-					free(object);
+					delete [] object;
 					object = new avf::Entry[1];
 					object[0].name = new char[1];
 					object[0].name[0]=0;
 					object[0].value = new char[200];
-					sprintf((char*)object[0].value, "line %ld: invalid character in variable declaration\n", line);
+					sprintf((char*)object[0].value, "line %ld: invalid character (\"%c\") in variable declaration\n", line, inC);
 					object[0].value[199]='\0';
 					return object;
 				}
 			case CR:
 				if(inC == CORRELATOR){
 					state = SV;
-					break;
+					inC = fgetc(target);
 				}
 				
 				else if(inC == BRACKETS[0]){
-					unsigned long outsize;
-					for(outsize = 0; object[outsize].name; ++outsize);
-
-					avf::Entry* holder = new avf::Entry[++outsize+1];
-					for(unsigned long i = 0; i < outsize; i++){
-						holder[i].name = object[i].name;
-						holder[i].value = object[i].value;
-						holder[i].parent = object[i].parent;
-					}
-					holder[outsize-1].name = objname;
-					holder[outsize-1].value = NULL;
-					holder[outsize].name = NULL;
+					outpos++; // record to output array
+					object[outpos-1].name = objname;
+					object[outpos-1].value = NULL;
+					object[outpos].name = NULL;
 					objname = NULL;
 					objnsize = 0;
-					free(object);
-					object = holder;//append the scanned values to output list
-					//NOTE: code above is a modified copy-paste from the EE state, mostly because we just need to create a new entry
-					if(!objects.empty())object[outsize-1].parent=objects.top(); // if theres something in the stack, put it in as a parent
-					else object[outsize-1].parent = NULL;
-					objects.push(&object[outsize-1]);
+					if(!objects.empty())object[outpos-1].parent=objects.top(); // if theres something in the stack, put it in as a parent
+					else object[outpos-1].parent = NULL;
+					objects.push(&object[outpos-1]);
 					state = SI; // rinse and repeat
-					break;
 				}
-
-				else if(isFrivolous(inC))break;
-				
+				else if(isFrivolous(inC));
 				else {
-					free(object);
+					delete []object;
 					object = new avf::Entry[1];
 					object[0].name = new char[1];
 					object[0].name[0]=0;
@@ -157,19 +161,23 @@ avf::Entry* avf::load(FILE* target){
 					object[0].value[199]='\0';
 					return object;
 				}
+				inC = fgetc(target);
+				break;
+
 			case SV:
-				if(isFrivolous(inC))break; // ignore anything that could not be an object
-				else if(inC == '\"'){
-					state = QU;
+				if(isFrivolous(inC)); // ignore anything that could not be an object
+				else if(inC == '\"')state = QU;
+				else {
+					state = OV;
 					break;
-				}	
-				else state = OV;
+				}
+				inC = fgetc(target);
+				break;
 
 			case OV:
 				if(inC == TERMINATOR){
 					objvalue [objvsize]='\0';
 					state=EE;
-					break;
 				}
 				else if(inC >= '0' && inC <= '9'){ // if our value is numeric
 					char* holder = new char[++objvsize+1]; // apppend it
@@ -177,12 +185,8 @@ avf::Entry* avf::load(FILE* target){
 					holder[objvsize-1]=inC;
 					if(objvsize > 1)free(objvalue);
 					objvalue=holder;
-					break;
 				}
-
-				else if(isFrivolous(inC)){
-					break;
-				}
+				else if(isFrivolous(inC));
 				else{
 					free(object);
 					object = new avf::Entry[1];
@@ -193,23 +197,20 @@ avf::Entry* avf::load(FILE* target){
 					object[0].value[199]='\0';
 					return object;
 				}
+				inC = fgetc(target);
+				break;
 			case QU:
-				if(inC == QUOTE){ // if second quotation mark is found
-					state = OV; // switch to end state: we know nothing more is here
-					break; // TODO: wacky. could cause bugs. implement seek to end state maybe???
-				}
-				if(inC == '\\'){ // if escape sign is encountered
-					state = QE;
-					break;
-				}
+				if(inC == QUOTE)state = OV; // switch to end state: we know nothing more is here
+				if(inC == '\\')state = QE; // switch to escape state
 				else {
 					char* holder = new char[++objvsize+1]; // apppend it
 					for(unsigned long i = 0; i < objvsize-1;i++)holder[i]=objvalue[i];
 					holder[objvsize-1]=inC;
 					if(objvsize > 1)free(objvalue);
 					objvalue=holder;
-					break;
 				}
+				inC = fgetc(target);
+				break;
 			case QE:{
 				char* holder = new char[++objvsize+1]; // apppend it
 				for(unsigned long i = 0; i < objvsize-1;i++)holder[i]=objvalue[i];
@@ -220,28 +221,21 @@ avf::Entry* avf::load(FILE* target){
 				break;
 			}
 			case EE:{
-					unsigned long outsize;
-					for(outsize = 0; object[outsize].name; ++outsize);
-					avf::Entry* holder = new avf::Entry[++outsize+1];
-					for(unsigned long i = 0; i < outsize; i++){
-						holder[i].name = object[i].name;
-						holder[i].value = object[i].value;
-						holder[i].parent = object[i].parent;
-					}
-					holder[outsize-1].name = objname;
-					holder[outsize-1].value = objvalue;
-					if(!objects.empty())holder[outsize-1].parent = objects.top();
-					else holder[outsize-1].parent = NULL;
-					holder[outsize].name = NULL;
-					objname = NULL;
-					objvalue = NULL;
-					objnsize = 0;
-					objvsize = 0;
-					free(object);
-					object = holder;//append the scanned values to output list
-					state = SI; // rinse and repeat
-					break;
-				}
+				outpos++;
+				#ifdef DEBUG
+				printf("creating entry %ld: \"%s\" = \"%s\"\n", outpos, objname, objvalue);
+				#endif
+				object[outpos-1].name = objname;
+				object[outpos-1].value = objvalue;
+				if(!objects.empty())object[outpos-1].parent = objects.top();
+				else object[outpos-1].parent = NULL;
+				objname = NULL;
+				objvalue = NULL;
+				objnsize = 0;
+				objvsize = 0;
+				state = SI; // rinse and repeat
+				break;
+			}
 		}
 	}
 }
@@ -263,34 +257,37 @@ unsigned long avf::write(FILE* target, avf::Entry* values){
 		#ifdef DEBUG
 		printf("elem: %ld\n", i);
 		#endif
-		char throwaway;
 		if(holder[i]){
 			#ifdef DEBUG
 			if(!layers.empty()){
 				printf("%ld\n%ld\n", holder[i]->parent, layers.top());
 			}
-			#endif
+			char throwaway;
 			scanf("%c", &throwaway);
-			if(!holder[i]->value){
-				#ifdef DEBUG
-				printf("object definition: %ld\n", holder[i]);
-				#endif
+			#endif
+			if(!holder[i]->value){ // if current entry has no value
 				char* tabs = new char[layers.size()+1];
 				for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
 				tabs[layers.size()]='\0';
 				outlen += fprintf(target, "%s%s %c\n", tabs, holder[i]->name, BRACKETS[0]);
 				
 				layers.push(holder[i]);
+				#ifdef DEBUG
+				printf("object definition: %ld\n", holder[i]);
+				printf("%ld, %ld\n", layers.top(), holder[i]);
+				#endif
 				holder[i]=NULL;
 				i = 0; //restart counting
 			}
-			else if(layers.empty() && !holder[i]->parent){//WHY IS THIS NOT WORKING???
+			else if(layers.empty() && !holder[i]->parent){
 				outlen += fprintf(target, "%s %c %s%c\n", holder[i]->name, CORRELATOR, holder[i]->value, TERMINATOR);
 				holder[i]=NULL;
 				i++;
 			}
-			else if( holder[i]->parent == layers.top() ){
+			else if( (*holder[i]).parent == layers.top() ){//WHY IS THIS NOT WORKING???
+				#ifdef DEBUG
 				printf("t1\n");
+				#endif
 				char* tabs = new char[layers.size()+1];
 				for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
 				tabs[layers.size()]='\0';
@@ -298,6 +295,7 @@ unsigned long avf::write(FILE* target, avf::Entry* values){
 				holder[i]=NULL;
 				i++;
 			}
+			else i++;
 			if( i == Elen - 1 && !layers.empty() ){
 				layers.pop();
 				char* tabs = new char[layers.size()+1];
@@ -309,7 +307,9 @@ unsigned long avf::write(FILE* target, avf::Entry* values){
 		}
 		else {
 			i++;
+			#ifdef DEBUG
 			printf("already processed. skipping...\n");
+			#endif
 		}
 		fflush(target);
 	}
