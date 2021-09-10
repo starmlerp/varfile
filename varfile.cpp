c909b15fe8d6f3379b5d656192f075a2810d5c9d
starmlerp <filip.jamuljak@gmail.com>
2021-09-10 23:49:59 +0200

    write WIP

--- varfile.cpp
+++ varfile.cpp
@@ -260,53 +260,46 @@ unsigned long avf::write(FILE* target, avf::Entry* values){
 			char throwaway;
 			scanf("%c", &throwaway);
 			#endif
-			if(layers.empty() && !holder[i]->parent){
-				if(!holder[i]->value){ // if current entry has no value
+
+			if(holder[i]->value){//if its a regular object
+					if(!holder[i]->parent && layers.empty()){
+						outlen += fprintf(target, "%s %c %s%c\n", holder[i]->name, CORRELATOR, holder[i]->value, TERMINATOR);
+						holder[i]=NULL;
+					}
+					else if(holder[i]->parent == layers.top()){
+						char* tabs = new char[layers.size()+1];
+						for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
+						tabs[layers.size()]='\0';
+						outlen += fprintf(target, "%s%s %c %s%c\n",tabs, holder[i]->name, CORRELATOR, holder[i]->value, TERMINATOR);
+						holder[i]=NULL;
+					}
+					i++;
+			}
+			else{//if its a nest
+				if(!holder[i]->parent && layers.empty()){
+					outlen += fprintf(target, "%s %c\n", holder[i]->name, BRACKETS[0]);
+				
+					#ifdef DEBUG
+					printf("object definition: %ld\n", holder[i]);
+					#endif
+					layers.push(holder[i]);
+					holder[i]=NULL;
+					
+				}
+				else if(holder[i]->parent == layers.top()){
 					char* tabs = new char[layers.size()+1];
 					for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
 					tabs[layers.size()]='\0';
 					outlen += fprintf(target, "%s%s %c\n", tabs, holder[i]->name, BRACKETS[0]);
-				
-					layers.push(holder[i]);
 					#ifdef DEBUG
 					printf("object definition: %ld\n", holder[i]);
 					#endif
-					holder[i]=NULL;
-					i = 0; //restart counting
-				}
-				else{
-					outlen += fprintf(target, "%s %c %s%c\n", holder[i]->name, CORRELATOR, holder[i]->value, TERMINATOR);
-					holder[i]=NULL;
-					i++;
-				}
-			}
-			else if( (*holder[i]).parent == layers.top()){
-				#ifdef DEBUG
-				printf("t1\n");
-				#endif
-				if(!holder[i]->value){ // if current entry has no value
-					char* tabs = new char[layers.size()+1];
-					for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
-					tabs[layers.size()]='\0';
-					outlen += fprintf(target, "%s%s %c\n", tabs, holder[i]->name, BRACKETS[0]);
-				
 					layers.push(holder[i]);
-					#ifdef DEBUG
-					printf("object definition: %ld\n", holder[i]);
-					#endif
 					holder[i]=NULL;
-					i = 0; //restart counting
 				}
-				else{
-					char* tabs = new char[layers.size()+1];
-					for(unsigned long j = 0; j < layers.size(); j++)tabs[j]='\t';
-					tabs[layers.size()]='\0';
-					outlen += fprintf(target, "%s%s %c %s%c\n",tabs, holder[i]->name, CORRELATOR, holder[i]->value, TERMINATOR);
-					holder[i]=NULL;
-					i++;
-				}
+				i = 0;
 			}
-			else i++;
+
 			if( i == Elen - 1 && !layers.empty() ){
 				#ifdef DEBUG
 				printf("loaded all objects in current layer. popping stack...\n");
